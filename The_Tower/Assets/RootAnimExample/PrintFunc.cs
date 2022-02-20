a6787c446898fc967af4d5d2e6c696c986eb20ee
Jared Goronkin <jgoronkin@gmail.com>
2022-02-19 19:22:38 -0500

    Root Animation And Functions With Anim Events

---
+++ The_Tower/Assets/RootAnimExample/PrintFunc.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PrintFunc : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CallFun()
+    {
+        print("The animator has called a function using animation events!");
+    }
+}
